41223fd6e4011f6a53d1aad731de467fd35465b1
wang jiankang <wjk@duofrom.com>
2020-07-02 16:11:37 +0800

    new api

---
+++ src/Request/Apis3rd/WxOpenAddCategory.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace VRobin\Weixin3rd\Request\Apis3rd;
+
+
+use VRobin\Weixin3rd\Request\Request3rd;
+
+class WxOpenAddCategory extends Request3rd
+{
+    protected $api = 'cgi-bin/wxopen/addcategory';
+    protected $method = 'POST';
+
+    public function setCategories($array){
+        $this->data['categories'] = $array;
+    }
+}
---
+++ src/Request/Apis3rd/WxOpenGetAllCategories.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace VRobin\Weixin3rd\Request\Apis3rd;
+
+
+use VRobin\Weixin3rd\Request\Request3rd;
+
+class WxOpenGetAllCategories extends Request3rd
+{
+    protected $api = 'cgi-bin/wxopen/getallcategories';
+}
